2d84a60489a9b5567136672702496ccfe3604d1f
jimmy fontaine <jim.chastain1@gmail.com>
2019-08-30 11:12:37 -0400

    updates

--- Example1-1.py
+++ Example1-1.py
@@ -1,7 +1,28 @@
-import matplotlib
-import matplotlib.pyplot as plt
+import os
 import numpy as np
 import pandas as pd 
 import sklearn.linear_model  
+import matplotlib
+import matplotlib.pyplot as plt
+
+from IPython import get_ipython
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+# mpl.rc('axes', labelsize=14)
+# mpl.rc('xtick', labelsize=12)
+# mpl.rc('ytick', labelsize=12)
+# Where to save the figures
+
+PROJECT_ROOT_DIR = "."
+CHAPTER_ID = "fundamentals"
+
+
+def save_fig(fig_id, tight_layout=True):
+    path = os.path.join(PROJECT_ROOT_DIR, "images", CHAPTER_ID, fig_id + ".png")
+    print("Saving figure", fig_id)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format='png', dpi=300)
 
 # Load the data with .csv
+
